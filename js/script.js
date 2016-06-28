31052916d21af63b1c13f27e381c099889324298
Carina Carriedo <carriedo.carina@gmail.com>
2016-06-28 11:02:34 -0700

    drawing to board works

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var board = ['X', '', 'O', '', '', '', 'X', '', '']
+var turn = 'X'
+
+function drawBoard(){
+  var squares = document.querySelectorAll('.square')
+  for (var i=0; i<9; i++){
+    squares[i].innerHTML = board[i]
+  }
+}
