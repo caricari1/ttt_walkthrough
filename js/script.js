5c23da1f29762a25e4a6daf281a187e506e6cc93
Carina Carriedo <carriedo.carina@gmail.com>
2016-06-28 11:20:05 -0700

    added switch turns

--- js/script.js
+++ js/script.js
@@ -1,9 +1,39 @@
-var board = ['X', '', 'O', '', '', '', 'X', '', '']
+// models
+var board = ['', '', '', '', '', '', '', '', '']
 var turn = 'X'
 
+// selectors
+var squares = document.querySelectorAll('.square')
+
+// set up click listeners for my squares
+for (var i=0; i<9; i++){
+  //loops through each square adding listener
+  squares[i].addEventListener('click', function(e) {
+    for (var j=0; j < 9; j ++) {
+      //looops through each square checking for click
+      if (squares[j] === e.target){
+        //updates model
+        board[j] = turn
+        //updates view
+        drawBoard()
+        switchTurn()
+      }
+    }
+  })
+}
+
+// drawing model to the screen
 function drawBoard(){
-  var squares = document.querySelectorAll('.square')
   for (var i=0; i<9; i++){
     squares[i].innerHTML = board[i]
+  }
+}
+
+// switching turns
+function switchTurn(){
+  if (turn === "X")
+    turn = "O"
+  else {
+    turn = "X"
   }
 }
